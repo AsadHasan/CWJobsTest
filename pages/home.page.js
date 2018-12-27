ff30dceaf91c93b38d5825a2eae892b7cc2d0ca8
Asad Hasan <Asad.Hasan@bjss.com>
2018-12-27 16:48:55 +0000

    removed classes, using functions instead

--- pages/home.page.js
+++ pages/home.page.js
@@ -1,19 +1,15 @@
-//var webdriver=require('selenium-webdriver');
-//var drv=new webdriver.Builder().forBrowser('chrome').build();
-//drv.get("");
-class Homepage{
-    searchBoxCSSLocator="input#keywords[name=Keywords][type=search]";
-    locationBoxCSSLocator="input#location[type=search]";
-    searchBoxSelector;
-    locationBoxSelector;
+// const webdriver = require('selenium-webdriver');
 
-    constructor (webdriver){
-        this.searchBoxSelector=webdriver.By.css(this.searchBoxCSSLocator);
-        this.locationBoxSelector=webdriver.By.css(this.locationBoxCSSLocator);
-    }
+const Homepage = function homepage(webdriver) {
+  this.driver = new webdriver.Builder().forBrowser('chrome').build();
+  this.searchBoxCSSLocator = 'input#keywords[name=Keywords][type=search]';
+  this.locationBoxCSSLocator = 'input#location[type=search]';
+  this.searchBoxSelector = this.driver.By.css(this.searchBoxCSSLocator);
+  this.locationBoxSelector = this.driver.By.css(this.locationBoxCSSLocator);
 
-    open(){
-        webdriver.get("https://www.cwjobs.co.uk/");
-    }
-}
-module.exports=Homepage;
+  function open() {
+    this.driver.get('https://www.cwjobs.co.uk/');
+  }
+};
+
+module.exports = Homepage;
--- specs/search.spec.js
+++ specs/search.spec.js
@@ -1,12 +1,13 @@
-var driver=require('selenium-webdriver');
-require('../pages/home.page');
+const driver = require('selenium-webdriver');
+const Homepage = require('../pages/home.page');
 
-describe('CW Jobs test', function(){
-    let webdriver=new driver.Builder().forBrowser('chrome').build();
+describe('CW Jobs test', () => {
+  // const webdriver = new driver.Builder().forBrowser('chrome').build();
+  // webdriver.findElement()
+  // this.webdriver = driver;
 
-    it('should open homepage',function(){
-        let homepage=new Homepage(webdriver);
-        homepage.open();
-    })
-})
-
+  it('should open homepage', () => {
+    const homepage = new Homepage(driver);
+    homepage.open();
+  });
+});
