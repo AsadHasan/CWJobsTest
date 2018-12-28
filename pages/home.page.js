549347609d3dce1e21367c091fcd5b0bfe8a8b34
Asad Hasan <Asad.Hasan@bjss.com>
2018-12-28 15:00:33 +0000

    added prettier formatter

--- pages/home.page.js
+++ pages/home.page.js
@@ -1,14 +1,14 @@
 class Homepage {
   constructor(webdriver) {
-    this.driver = new webdriver.Builder().forBrowser('chrome').build();
-    this.searchBoxCSSLocator = 'input#keywords[name=Keywords][type=search]';
-    this.locationBoxCSSLocator = 'input#location[type=search]';
+    this.driver = new webdriver.Builder().forBrowser("chrome").build();
+    this.searchBoxCSSLocator = "input#keywords[name=Keywords][type=search]";
+    this.locationBoxCSSLocator = "input#location[type=search]";
     this.searchBoxSelector = webdriver.By.css(this.searchBoxCSSLocator);
     this.locationBoxSelector = webdriver.By.css(this.locationBoxCSSLocator);
   }
 
   open() {
-    this.driver.get('https://www.cwjobs.co.uk/');
+    this.driver.get("https://www.cwjobs.co.uk/");
   }
 }
 
--- specs/search.spec.js
+++ specs/search.spec.js
@@ -1,12 +1,12 @@
-import driver from 'selenium-webdriver';
-import Homepage from '../pages/home.page';
+import driver from "selenium-webdriver";
+import Homepage from "../pages/home.page";
 
-describe('CW Jobs test', () => {
+describe("CW Jobs test", () => {
   // const webdriver = new driver.Builder().forBrowser('chrome').build();
   // webdriver.findElement()
   // this.webdriver = driver;
 
-  it('should open homepage', () => {
+  it("should open homepage", () => {
     const homepage = new Homepage(driver);
     homepage.open();
   });
